1b29e18280cef6b7bf79c769d4f960095bf19ca0
HUAWEI <HUAWEI@Engineer-Baraa-A>
2022-05-12 06:07:17 +0300

    init

---
+++ Hospital.java
@@ -0,0 +1,27 @@
+package kaau1;
+import java.util.ArrayList;
+public class Hospital {
+
+	private ArrayList<String> clinic = new ArrayList<String>();
+	private ArrayList<String> patientsfile = new ArrayList<String>();
+	public Hospital(ArrayList<String> clinic, ArrayList<String> patientsfile) {
+		super();
+		this.clinic = clinic;
+		this.patientsfile = patientsfile;
+		
+		
+		clinic.add("heart clinic");
+		clinic.add("Dental clinic");
+		clinic.add("Surgery clinic");
+		clinic.add("Orthopedic Clinic");
+		clinic.add("Kids Clinic");
+		clinic.add("Internal Medicine Clinic");
+		clinic.add("Dermatology clinic");
+		System.out.println(clinic.toString());
+	}
+
+	public Hospital() {
+
+	}
+	////asd
+}
